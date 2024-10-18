ffb1180a788a3f4196b777441d03bb99a68017cc
Fabis-li <fabiano_slima@hotmail.com>
2024-10-18 19:46:53 -0300

    implementadas notificacoes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShellComponent } from "./core/shell/shell.component";
+import { Observable } from 'rxjs';
+import { UsuarioTokenViewModel } from './core/auth/models/auth.models';
+import { UsuarioService } from './core/auth/services/usuario.service';
+import { NotificacaoService } from './core/notificacao/notificacao.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +12,22 @@ import { ShellComponent } from "./core/shell/shell.component";
   imports: [RouterOutlet, ShellComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  title = 'e-agenda';
+export class AppComponent implements OnInit {
+  usuarioAutenticado$?: Observable<UsuarioTokenViewModel | undefined>;
+
+  constructor(private usuarioService: UsuarioService,
+    private notificacaoService: NotificacaoService,
+  ) { }
+
+  ngOnInit(): void {
+    this.usuarioAutenticado$ = this.usuarioService.usuarioAutenticado;
+
+  }
+
+  efetuarLogout() {
+    this.usuarioService.logout();
+
+    this.notificacaoService.sucesso('Logout efetuado com sucesso!');
+  }
+
 }
