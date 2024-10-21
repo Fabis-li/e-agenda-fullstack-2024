d9fb07d03719147ca18a61d652118acf77b6c30f
Fabis-li <fabiano_slima@hotmail.com>
2024-10-20 22:32:26 -0300

    implementacao autenticacao usuario

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { Observable } from 'rxjs';
 import { UsuarioTokenViewModel } from './core/auth/models/auth.models';
 import { UsuarioService } from './core/auth/services/usuario.service';
 import { NotificacaoService } from './core/notificacao/notificacao.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ShellComponent],
+  imports: [RouterOutlet, ShellComponent, AsyncPipe],
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
