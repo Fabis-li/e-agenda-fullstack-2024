a40b3c0caa748398f459927934a08ab53c885ba6
Fabis-li <fabiano_slima@hotmail.com>
2024-10-24 10:06:34 -0300

    criado componente de listagem de categorias

---
+++ client/src/app/views/categorias/models/categoria.models.ts
@@ -0,0 +1,14 @@
+export interface ListagemCategoria {
+  id: string;
+  titulo: string;
+}
+
+export interface CadastrarCategoria {
+  titulo: string;
+}
+
+
+export interface Categoriacriada {
+  id: string;
+  titulo: string;
+}
