b2e111b0884579e06699b80a9bd95b216d88e845
Fabis-li <fabiano_slima@hotmail.com>
2024-10-29 15:27:12 -0300

    criado cadastro de tarefas

---
+++ client/src/app/views/tarefas/cadastrar/cadastro-tarefa.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-tarefa',
+  standalone: true,
+  imports: [],
+  templateUrl: './cadastro-tarefa.component.html'
+})
+export class CadastroTarefaComponent {
+
+}
