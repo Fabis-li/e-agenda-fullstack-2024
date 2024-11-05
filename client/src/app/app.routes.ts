58fa45707f240e82c3b962eb33509126b6a25b0c
Fabis-li <fabiano_slima@hotmail.com>
2024-11-05 11:30:50 -0300

    aplicando lazyload na app

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -12,47 +12,91 @@ import { categoriasRoutes } from './views/categorias/categorias.routes';
 import { despesasRoutes } from './views/despesas/despesas.routes';
 
 const authGuard: CanMatchFn = (): Observable<boolean | UrlTree> => {
-
   const router = inject(Router);
   const usuarioService = inject(UsuarioService);
 
-
-  return usuarioService.usuarioAutenticado
-   .pipe(map(usuario => {
-      if(!usuario)  return router.parseUrl('/login');
+  return usuarioService.usuarioAutenticado.pipe(
+    map((usuario) => {
+      if (!usuario) return router.parseUrl('/login');
 
       return true;
-   })) // Verificando se o usuário está autenticado e redirecionando para a tela de login caso não esteja
-}
+    })
+  ); // Verificando se o usuário está autenticado e redirecionando para a tela de login caso não esteja
+};
 
 const authUserGuard: CanMatchFn = (): Observable<boolean | UrlTree> => {
-
   const router = inject(Router);
   const usuarioService = inject(UsuarioService);
 
-
-  return usuarioService.usuarioAutenticado
-   .pipe(map(usuario => {
-      if(usuario) return router.parseUrl('/dashboard');
+  return usuarioService.usuarioAutenticado.pipe(
+    map((usuario) => {
+      if (usuario) return router.parseUrl('/dashboard');
 
       return true;
-   }))// Verificando se o usuário está autenticado e redirecionando para a tela de login caso não esteja
-}
+    })
+  ); // Verificando se o usuário está autenticado e redirecionando para a tela de login caso não esteja
+};
 
 export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {
-    path:'dashboard',
-    component: DashboardComponent,
-    canMatch: [authGuard] // Adicionando a guarda de rota
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./views/dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+    canMatch: [authGuard], // Adicionando a guarda de rota
   },
 
-  { path: 'registro', component: RegistroComponent, canMatch: [authUserGuard] },
-  { path: 'login', component: LoginComponent,canMatch: [authUserGuard] },
+  {
+    path: 'registro',
+    loadComponent: () =>
+      import('./core/auth/views/registro/registro.component').then(
+        (c) => c.RegistroComponent
+      ),
+    canMatch: [authUserGuard],
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./core/auth/views/login/login.component').then(
+        (c) => c.LoginComponent
+      ),
+    canMatch: [authUserGuard],
+  },
 
-  { path: 'contatos', children: contatosRoutes},
-  { path: 'compromissos', children: compromissosRoutes},
-  { path: 'tarefas', children: tarefasRoutes},
-  { path: 'categorias', children: categoriasRoutes},
-  { path: 'despesas', children: despesasRoutes}
+  {
+    path: 'contatos',
+    loadChildren: () =>
+      import('./views/contatos/contatos.routes').then((m) => m.contatosRoutes),
+    canMatch: [authGuard],
+  },
+  {
+    path: 'compromissos',
+    loadChildren: () =>
+      import('./views/compromissos/compromissos.routes').then(
+        (m) => m.compromissosRoutes
+      ),
+    canMatch: [authGuard],
+  },
+  {
+    path: 'tarefas',
+    loadChildren: () =>
+      import('./views/tarefas/tarefas.routes').then((m) => m.tarefasRoutes),
+    canMatch: [authGuard],
+  },
+  {
+    path: 'categorias',
+    loadChildren: () =>
+      import('./views/categorias/categorias.routes').then(
+        (m) => m.categoriasRoutes
+      ),
+    canMatch: [authGuard],
+  },
+  {
+    path: 'despesas',
+    loadChildren: () =>
+      import('./views/despesas/despesas.routes').then((m) => m.despesasRoutes),
+    canMatch: [authGuard],
+  },
 ];
